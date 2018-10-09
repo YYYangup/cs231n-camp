0afc2e2136b07ba758999691d94d6f199fbbc3de
YYYangup <43463565+YYYangup@users.noreply.github.com>
2018-10-09 10:19:01 +0800

    Add files via upload

---
+++ assignment/assignment1/1.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class Matrix(object):
+	def __init__(self):
+		pass
+	def mul(self,w,x):
+		y = []
+		for asix in range(w.shape[0]):
+			w_num = w[asix,:]
+			for ysix in range(x.shape[1]):
+				x_num = x[:,ysix]
+				w_x_sum = 0
+				for i in range(w.shape[1]):
+					w_x_sum += w_num[i]*x_num[i]
+				y.append(w_x_sum)
+		return y
+w_input = np.array([[1,2,3],[4,5,6]],dtype = np.int)
+x_input = np.array([[1,2],[4,5],[6,7]],dtype = np.int)
+
+m = Matrix()
+y_output = m.mul(w_input,x_input)
+y_output = np.array(y_output).reshape(w_input.shape[0],x_input.shape[1])
+print(y_output)
+
