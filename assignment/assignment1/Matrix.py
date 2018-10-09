f3465ff0d0bb6932a915aaeaa39bf298e5f78731
YYYangup <43463565+YYYangup@users.noreply.github.com>
2018-10-09 10:34:39 +0800

    Update Matrix.py

--- assignment/assignment1/Matrix.py
+++ assignment/assignment1/Matrix.py
@@ -1,18 +1,18 @@
 import numpy as np
 
 class Matrix(object):
-	def __init__(self):
-		pass
-	def mul(self,w,x):
-		y = []
-		for asix in range(w.shape[0]):
-			w_num = w[asix,:]
-			for ysix in range(x.shape[1]):
-				x_num = x[:,ysix]
-				w_x_sum = 0
-				for i in range(w.shape[1]):
-					w_x_sum += w_num[i]*x_num[i]
-				y.append(w_x_sum)
+  def __init__(self):
+    pass
+  def mul(self,w,x):
+    y = []
+    for asix in range(w.shape[0]):
+      w_num = w[asix,:]
+      for ysix in range(x.shape[1]):
+        x_num = x[:,ysix]
+        w_x_sum = 0
+        for i in range(w.shape[1]):
+          w_x_sum += w_num[i]*x_num[i]
+    y.append(w_x_sum)
 		return y
 w_input = np.array([[1,2,3],[4,5,6]],dtype = np.int)
 x_input = np.array([[1,2],[4,5],[6,7]],dtype = np.int)
